daba0c2f8bda806740a1b1a43f3233b85b33a2df
mzhub <mishabhadhi@gmail.com>
2024-12-12 22:26:43 +0530

    scaling

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,9 +24,9 @@ df["loan_intent"] = df["loan_intent"].replace({"Education" :0, "Medical" : 1, "V
 x = df.drop("loan_status", axis = 1)
 y = df["loan_status"]
 
-# from sklearn.preprocessing import StandardScaler
-# ss01 = StandardScaler()
-# x = ss01.fit_transform(x)
+from sklearn.preprocessing import StandardScaler
+ss01 = StandardScaler()
+x = ss01.fit_transform(x)
 
 from sklearn.neighbors import KNeighborsClassifier
 knn = KNeighborsClassifier(metric = 'manhattan', n_neighbors = 11, weights = 'distance')
