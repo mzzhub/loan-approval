17ccfeb7d4e7f673d99f92a7e7d73dbc9afc09ac
mzhub <mishabhadhi@gmail.com>
2024-12-12 14:33:08 +0530

    ml

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,15 +34,15 @@ knn.fit(x, y)
 
 # user inputs
 age = int(st.slider("**Age**", 20, 114, 30))
-gender = str(st.radio("Gender", ("Male", "Female")))
-education = str(st.selectbox("Education", ("Bachelor", "Associate", "High School", "Master", "Doctorate")))
-income = int(st.number_input("Annual Income", 8000, 7200000, 50000, 500))
-emp_exp = int(st.number_input("Employment Experience in Years", 0, 125, 5))
-home = str(st.radio("House Ownership", ("Rent", "Mortgage", "Own", "Other")))
-amount = int(st.number_input("Requried Loan Amount", 500, 35000, 10000, 500))
-purpose = str(st.selectbox("Purpose", ("Education", "Medical", "Venture", "Personal", "Debt Consolidation", "Home Improvement")))
-rate = float(st.number_input("Intrest Rate", 5.50, 20.00, 6.00, 0.01))
-score = int(st.number_input("Current Credit Score", 390, 850, 400))
+gender = str(st.radio("**Gender**", ("Male", "Female")))
+education = str(st.selectbox("**Education**", ("Bachelor", "Associate", "High School", "Master", "Doctorate")))
+income = int(st.number_input("**Annual Income**", 8000, 7200000, 50000, 500))
+emp_exp = int(st.number_input("**Employment Experience in Years**", 0, 125, 5))
+home = str(st.radio("**House Ownership**", ("Rent", "Mortgage", "Own", "Other")))
+amount = int(st.number_input("**Requried Loan Amount**", 500, 35000, 10000, 500))
+purpose = str(st.selectbox("**Purpose**", ("Education", "Medical", "Venture", "Personal", "Debt Consolidation", "Home Improvement")))
+rate = float(st.number_input("**Intrest Rate**", 5.50, 20.00, 6.00, 0.01))
+score = int(st.number_input("**Current Credit Score**", 390, 850, 400))
 
 user_input = {
     'person_age': [age],
