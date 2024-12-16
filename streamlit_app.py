a6c53bcc5bbf276db2abf57c37e3f758e1167c1a
mzhub <mishabhadhi@gmail.com>
2024-12-16 21:11:47 +0530

    ml

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,12 +80,11 @@ st.subheader("Prediction")
 import numpy as np
 possible_output = np.array(["Eligible", "Ineligible"])
 
-# st.success(possible_output[pred][0])
 
-if pred[0] == 0:  # Assuming 0 corresponds to "Eligible"
-    st.success(possible_output[pred][0])  # Green background
-else:  # Assuming 1 corresponds to "Ineligible"
-    st.error(possible_output[pred][0])  # Red background
+if pred[0] == 0:
+    st.success(possible_output[pred][0])
+else:
+    st.error(possible_output[pred][0])
 
 st.dataframe(prob_df, column_config = {
                                         "Eligible" : st.column_config.ProgressColumn(
